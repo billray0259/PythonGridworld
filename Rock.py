d9aac76ea0af7a82cc893d23b19726277987403b
Bill Ray <31375073+billray0259@users.noreply.github.com>
2017-12-05 11:47:02 -0700

    Deleting duplicate files

--- Rock.py
+++
@@ -1,8 +0,0 @@
-from Actor import Actor
-
-class Rock(Actor):
-    def __init__(self, location, grid):
-        Actor.__init__(self, location, grid, icon="rock.png")
-
-    def act(self):
-        pass
