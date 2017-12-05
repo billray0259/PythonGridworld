b5c2eeb7583eb0ebd6830cac8443eae0ed77d8d2
Bill Ray <31375073+billray0259@users.noreply.github.com>
2017-12-05 11:46:00 -0700

    Deleting duplicate files

--- Flower.py
+++
@@ -1,9 +0,0 @@
-from Actor import Actor
-
-
-class Flower(Actor):
-    def __init__(self, location, grid):
-        Actor.__init__(self, location, grid, icon="car.png")
-
-    def act(self):
-        pass
